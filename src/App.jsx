0815233b4a0cd349c807423cbe2e9641097de8fa
williamrolando88 <william.rolando88@hotmail.com>
2021-12-27 21:42:43 -0500

    Setup vite-react-app and tailwindcss

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export class App extends Component {
+  render() {
+    return <div className='text-3xl'>Hello world!</div>;
+  }
+}
+
+export default App;
